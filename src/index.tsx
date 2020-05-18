5d4538b92078448824d99e021c7aa21f9d13bdf1
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2020-05-18 16:57:59 -0500

    Initial branch (#1)
    * Adds base documentation
    
    * Adds gh actions
    
    * Organize dependencies
    
    * Add linter
    
    * Linting
    
    * Adds router
    
    * Organice folders
    
    * Adds apollo

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { render } from "react-dom";
+import { Router } from "@reach/router";
 
-ReactDOM.render(
+import App from "./views/App";
+import * as serviceWorker from "./serviceWorker";
+import "./scss/index.scss";
+
+render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App path="/" />
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
